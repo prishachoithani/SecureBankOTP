cddeaef6eb5eb67a11984fb17ccdcbba58c67c41
prishachoithani <prishachoithani@gmail.com>
2026-09-16 23:05:18 -0700

    FraudDetectedException class

--- src/com/securebank/exceptions/FraudDetectedException.java
+++ src/com/securebank/exceptions/FraudDetectedException.java
@@ -1,6 +1,5 @@
 package com.securebank.exceptions;
 
-/** Thrown when the fraud detection engine blocks a transaction before it is completed. */
 public class FraudDetectedException extends Exception {
     public FraudDetectedException(String message) {
         super(message);
