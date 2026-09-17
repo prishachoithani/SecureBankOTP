de18356623b1595eda6e9316e14e8d788905c38d
prishachoithani <prishachoithani@gmail.com>
2026-09-16 23:06:03 -0700

    InvalidOTPException class

--- src/com/securebank/exceptions/InvalidOTPException.java
+++ src/com/securebank/exceptions/InvalidOTPException.java
@@ -1,6 +1,4 @@
 package com.securebank.exceptions;
-
-/** Thrown when a submitted OTP code does not match the one issued for a transaction. */
 public class InvalidOTPException extends Exception {
     public InvalidOTPException(String message) {
         super(message);
