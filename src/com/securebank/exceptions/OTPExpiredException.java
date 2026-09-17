927d4148a67d5efb5f37e8096bde9f91edfb9bf0
prishachoithani <prishachoithani@gmail.com>
2026-09-16 23:06:33 -0700

    OTPExpiredException class

--- src/com/securebank/exceptions/OTPExpiredException.java
+++ src/com/securebank/exceptions/OTPExpiredException.java
@@ -1,6 +1,4 @@
 package com.securebank.exceptions;
-
-/** Thrown when a user attempts to verify an OTP after its validity window has elapsed. */
 public class OTPExpiredException extends Exception {
     public OTPExpiredException(String message) {
         super(message);
