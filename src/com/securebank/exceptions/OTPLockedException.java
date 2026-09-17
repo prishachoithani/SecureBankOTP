db686bdc36baf6ed3e9f9011263713e4414ff5c1
prishachoithani <prishachoithani@gmail.com>
2026-09-16 23:06:54 -0700

    OTPLockedException class

--- src/com/securebank/exceptions/OTPLockedException.java
+++ src/com/securebank/exceptions/OTPLockedException.java
@@ -1,6 +1,4 @@
 package com.securebank.exceptions;
-
-/** Thrown when too many incorrect OTP attempts have been made for a transaction. */
 public class OTPLockedException extends Exception {
     public OTPLockedException(String message) {
         super(message);
