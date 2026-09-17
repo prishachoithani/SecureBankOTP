d6c955cb5ac54cb58dfe6f95e928d715d7ea675a
prishachoithani <prishachoithani@gmail.com>
2026-09-16 23:05:40 -0700

    InsufficientFundsException

--- src/com/securebank/exceptions/InsufficientFundsException.java
+++ src/com/securebank/exceptions/InsufficientFundsException.java
@@ -1,6 +1,4 @@
 package com.securebank.exceptions;
-
-/** Thrown when a withdrawal/transfer exceeds the available account balance. */
 public class InsufficientFundsException extends Exception {
     public InsufficientFundsException(String message) {
         super(message);
